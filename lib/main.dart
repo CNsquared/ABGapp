2e2e4f0a8ba562d14e3e62a99e3d6b687b6eab69
CNsquared <aoshima@ucsd.edu>
2023-07-09 17:11:54 -0700

    Formatting text fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -7,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Form Styling Demo';
+    const appTitle = 'ABG App';
     return MaterialApp(
       title: appTitle,
       home: Scaffold(
@@ -31,19 +33,41 @@ class MyCustomForm extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
           child: TextFormField(
+            style: TextStyle(fontSize: 20),
             decoration: const InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Tax',
+              prefixIcon: Padding(
+                        padding: EdgeInsetsDirectional.only(start: 20, top: 10),
+                        child: Text("\$",
+                                    style: TextStyle(fontSize: 25),)
+                        ), 
             ),
+            keyboardType: const TextInputType.numberWithOptions(decimal: true),
+            inputFormatters:[ 
+              FilteringTextInputFormatter.allow(RegExp(r'^(\d+)?\.?\d{0,2}')),
+             ],
+        
+
           ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
           child: TextFormField(
+            style: TextStyle(fontSize: 20),
             decoration: const InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Tip',
+              prefixIcon: Padding(
+                        padding: EdgeInsetsDirectional.only(start: 20, top: 10),
+                        child: Text("\$",
+                                    style: TextStyle(fontSize: 25),)
+                        ), 
             ),
+            keyboardType: const TextInputType.numberWithOptions(decimal: true),
+            inputFormatters: [ FilteringTextInputFormatter.allow(RegExp(r'^(\d+)?\.?\d{0,2}')),
+            ],
+            
           ),
         ),
       ],
